e880b5850ad019c08077c444a8db71d8ca788aad
AC-2K <aldair.chutumia@gmail.com>
2024-01-26 00:04:16 +0200

    Autenticacao melhorado

--- Autenticacao.php
+++ Autenticacao.php
@@ -2,6 +2,9 @@
     // Start the session
     session_start();
     $link = mysqli_connect('localhost','root','','pfc');
+    $somaAdmin = 0;
+    $somaTecnico = 0;
+    $somaGestor = 0;
 
     
     $user = $_POST['username'];
@@ -29,8 +32,12 @@
                     echo 'window.location.href = "mainPage.php";';
                     echo '</script>';
                 }
-            }            
+            }
+            goto gestor;          
         } else {
+            //GOTO gestor
+            gestor:
+
             $_SESSION["usuario"] = 1;
             $stmt = $link->prepare(" SELECT * from gestor where `ges_nome` = ?");
             $stmt->bind_param("s", $user);
@@ -49,16 +56,20 @@
                         echo 'window.location.href = "mainPage.php";';
                         echo '</script>';
                     }
-                } 
-
+                }
+                goto admin;    
             }else {
+                //GOTO Admin 
+                admin:
+
                 $_SESSION["usuario"] = 2;
                 $stmt = $link->prepare(" SELECT * from admin where `ad_user` = ?");
                 $stmt->bind_param("s", $user);
                 $stmt->execute();
                 $result = $stmt->get_result();
                 $data = [];
-                $data = $result->fetch_all(MYSQLI_ASSOC);  
+                $data = $result->fetch_all(MYSQLI_ASSOC); 
+
 
 
                 if (mysqli_num_rows($result) == 1) {
@@ -72,12 +83,21 @@
                             echo '</script>';
                         }
                     } 
-                }else{
+                }
+
+                //Caso todos casos forem negados, serao rejeitados do programa
+
+                $somaGestor++;
+                $somaAdmin++;
+                $somaTecnico++; 
+
+                if ($somaAdmin > 0 && $somaGestor > 0 && $somaTecnico > 0 ) {
                     echo '<script type="text/javascript">';
                     echo 'alert("Usuario invalido");';;
                     echo 'window.location.href = "sair.php";';
                     echo '</script>';
-                }
+                } 
+                
             }
         }
 
