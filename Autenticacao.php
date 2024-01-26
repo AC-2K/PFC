da8a20ef66547d4c3ae37753e2a988c36f2bf09d
AC-2K <aldair.chutumia@gmail.com>
2024-01-26 13:19:50 +0200

    Controle de autenticacao melhorado

--- Autenticacao.php
+++ Autenticacao.php
@@ -3,9 +3,8 @@
     session_start();
     $link = mysqli_connect('localhost','root','','pfc');
     $somaAdmin = 0;
-    $somaTecnico = 0;
     $somaGestor = 0;
-
+    $somaTecnico = 0;
     
     $user = $_POST['username'];
     $pass = $_POST['pass'];
@@ -24,7 +23,6 @@
           
         if (mysqli_num_rows($result) == 1) {
             
-
             foreach($data as $datas){ 
                 
                 $hashed_password = $datas['tec_pass'];
@@ -37,72 +35,66 @@
                     echo '</script>';
                 }
             }
-            $somaTecnico = 1;
-                    
-        } else {
-            //GOTO gestor
-            
-
-            $_SESSION["usuario"] = 1;
-            $stmt = $link->prepare(" SELECT * from gestor where `ges_nome` = ?");
-            $stmt->bind_param("s", $user);
-            $stmt->execute();
-            $result = $stmt->get_result();
-            $data = [];
-            $data = $result->fetch_all(MYSQLI_ASSOC);  
-
-            if (mysqli_num_rows($result) == 1) {
-                foreach($data as $datas){ 
-                    $hashed_password = $datas['ges_pass'];
-    
-                    if(password_verify($pass, $hashed_password)) {
-                        echo '<script type="text/javascript">';
-                        echo 'alert("Bem vindo - Gestor");';;
-                        echo 'window.location.href = "mainPage.php";';
-                        echo '</script>';
-                    }
-                }
-                $somaGestor = 1;
-                 
-            }else {
-                //GOTO Admin 
-                
-
-                $_SESSION["usuario"] = 2;
-                $stmt = $link->prepare(" SELECT * from admin where `ad_user` = ?");
-                $stmt->bind_param("s", $user);
-                $stmt->execute();
-                $result = $stmt->get_result();
-                $data = [];
-                $data = $result->fetch_all(MYSQLI_ASSOC); 
-
-
-
-                if (mysqli_num_rows($result) == 1) {
-                    foreach($data as $datas){ 
-                        $hashed_password = $datas['ad_pass'];
         
-                        if(password_verify($pass, $hashed_password)) {
-                            echo '<script type="text/javascript">';
-                            echo 'alert("Bem vindo - Admin");';;
-                            echo 'window.location.href = "mainPage.php";';
-                            echo '</script>';
-                        }
-                    } 
-                    $somaAdmin = 1;
-                }
-
-                //Caso todos casos forem negados, serao rejeitados do programa
-
-
-                if ($somaAdmin > 0 && $somaGestor > 0 && $somaTecnico > 0 ) {
-                    echo '<script type="text/javascript">';
-                    echo 'alert("Usuario invalido");';;
-                    echo 'window.location.href = "sair.php";';
-                    echo '</script>';
-                } 
-                
-            }
         }
+        $somaTecnico = 1; 
+        
+    $stmt2 = $link->prepare(" SELECT * from gestor where `ges_nome` = ?");
+    $stmt2->bind_param("s", $user);  
+    $stmt2->execute();
+    $result2 = $stmt2->get_result();
+    $data2 = [];
+    $data2 = $result2->fetch_all(MYSQLI_ASSOC); 
+        if (mysqli_num_rows($result2) == 1) {
+            $_SESSION["usuario"] = 1;
+            foreach($data2 as $datas){ 
+                
+                $hashed_password = $datas['ges_pass'];
+
+                if(password_verify($pass, $hashed_password)) {
+                    
+                    echo '<script type="text/javascript">';
+                    echo 'alert("Bem vindo - Gestor");';;
+                    echo 'window.location.href = "mainPage.php";';
+                    echo '</script>';
+                }
+            }
+            
+        }
+        $somaGestor = 1;
+
+    $stmt3 = $link->prepare(" SELECT * from admin where `ad_user` = ?");
+    $stmt3->bind_param("s", $user);
+    $stmt3->execute();
+    $result3 = $stmt3->get_result();
+    $data3 = [];
+    $data3 = $result3->fetch_all(MYSQLI_ASSOC); 
+        if (mysqli_num_rows($result3) == 1) {
+            $_SESSION["usuario"] = 2;
+            foreach($data3 as $datas){ 
+                
+                $hashed_password = $datas['ad_pass'];
+
+                if(password_verify($pass, $hashed_password)) {
+                    
+                    echo '<script type="text/javascript">';
+                    echo 'alert("Bem vindo - Admin");';;
+                    echo 'window.location.href = "mainPage.php";';
+                    echo '</script>';
+                }
+            }
+            
+        }
+        $somaAdmin = 1;
+          
+        if ($somaAdmin > 0 && $somaGestor > 0 && $somaTecnico > 0 ) {
+            echo '<script type="text/javascript">';
+            echo 'alert("Usuario invalido");';;
+            echo 'window.location.href = "sair.php";';
+            echo '</script>';
+        } 
+    echo $somaAdmin;
+    echo $somaGestor;
+    echo $somaTecnico;
 
 ?>
